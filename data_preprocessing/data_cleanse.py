9ad01806918e9cb351fc8edce8bd520e8318f29e
Moz125 <FA16-BCS-021@ISBSTUDENT.COMSATS.EDU.PK>
2020-06-18 16:44:51 +0500

    upload and clean book
    added revised book review clean code and the upload book code

--- data_preprocessing/data_cleanse.py
+++ data_preprocessing/data_cleanse.py
@@ -5,6 +5,24 @@ import os.path
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 
+genres = ['10th-century', '11th-century', '12th-century', '13th-century', '14th-century', 
+'15th-century', '16th-century', '17th-century', '1864-shenandoah-campaign', '18th-century', 
+'1917', '19th-century', '1st-grade', '20th-century', '21st-century', '2nd-grade', '40k', 
+'ableism', 'abuse', 'academia', 'academic', 'academics', 'accounting', 'accra', 'action', 
+'activism', 'adaptations', 'addis-ababa', 'addition', 'adolescence', 'adoption', 'adult', 
+'adult-colouring-books', 'adult-fiction', 'adventure', 'adventurers', 'aeroplanes', 'africa', 
+'african-american', 'african-american-literature', 'african-american-romance', 
+'african-literature', 'agender', 'agriculture', 'ahistory', 'aircraft', 'airliners', 
+'airships', 'albanian-literature', 'alchemy', 'alcohol', 'alexandria', 'algebra', 'algeria', 
+'algiers', 'algorithms', 'aliens', 'alternate-history', 'alternate-universe', 
+'alternative-medicine', 'amateur-sleuth', 'amazon', 'ambulance-service', 'ambulances', 
+'american', 'american-civil-war', 'american-classics', 'american-fiction', 'american-history', 
+'american-novels', 'american-revolution', 'american-revolutionary-war', 'americana', 'amish', 
+'amish-fiction', 'amish-historical-romance-fiction', 'ancient', 'ancient-history', 'androgyne',
+'angels', 'anglo-saxon', 'angola', 'animal-fiction', 'animals', 'anime', 'anthologies', 
+'anthropology', 'anthropomorphic', 'anti-intellectualism', 'anti-racist', 'anti-science', 
+'antietam-campaign', 'antiquities', 'antisemitism', 'apocalyptic', 'apple']
+
 #removing reviews that are not english
 def review_cleanse(reviews):
     cleaned_reviews = []
@@ -36,16 +54,27 @@ def book_cleanse(books_data):
     return cleansed
 
 #reading book data for cleansing
-def read_book_cleanse(books_data):    
-    for genre in books_data:
+def read_book_cleanse(genres):    
+    for genre in genres:
+        if genre in ['10th-century', '11th-century', '12th-century', '13th-century', '14th-century', 
+'15th-century', '16th-century', '17th-century', '1864-shenandoah-campaign', '18th-century', 
+'1917', '19th-century', '1st-grade', '20th-century', '21st-century', '2nd-grade', '40k', 
+'ableism', 'abuse', 'academia', 'academic', 'academics', 'accounting', 'accra', 'action', 
+'activism', 'adaptations', 'addis-ababa', 'addition', 'adolescence', 'adoption', 'adult', 
+'adult-colouring-books', 'adult-fiction', 'adventure', 'adventurers', 'aeroplanes', 'africa', 
+'african-american', 'african-american-literature', 'african-american-romance', 
+'african-literature', 'agender', 'agriculture', 'ahistory', 'aircraft', 'airliners', 
+'airships', 'albanian-literature', 'alchemy', 'alcohol', 'alexandria', 'algebra', 'algeria', 'algiers']:
+            continue
         parts = len(os.listdir('books/' + genre))
 
         if not os.path.exists("cleansed_books/" + genre):
             os.mkdir("cleansed_books/" + genre)
+            print("DIR =====> ", genre)
 
-        for x in range(1, parts+1):
+        for x in range(1, parts):
             part = str(x)
-
+            print("part ", x)
             with open("books/" + genre + "/part_" + part + ".json", encoding="utf8") as read_file:
                 books_data = json.load(read_file)
                 cleansed_books = book_cleanse(books_data)
@@ -53,16 +82,5 @@ def read_book_cleanse(books_data):
             with open("cleansed_books/" + genre + "/part_" + part + ".json", "w", encoding="utf8") as write_file:
                 json.dump(cleansed_books, write_file, indent=4, ensure_ascii=False)
 
-#reading bookshelf list
-with open("book_list.json", encoding="utf8") as read_file:
-    book_data = json.load(read_file)
 
-print('cleansing book')
-read_book_cleanse(book_data)
-
-
-
-
-
-
-    
+read_book_cleanse(genres)
