42ebdbf08ad0fc3e3ad8c4775acf53ecfb282da4
Moz125 <FA16-BCS-021@ISBSTUDENT.COMSATS.EDU.PK>
2020-07-21 21:13:11 +0500

    fixed genre css

--- client/src/components/Genres/GenreResult.js
+++ client/src/components/Genres/GenreResult.js
@@ -26,86 +26,86 @@ export class GenreResult extends Component {
         <div style={{borderBottom:'1.5px solid #151B2D', backgroundColor: '#151B2D'}}>
                 <h4 className="font-weight-light" style={{color: 'white', fontSize: '27px', paddingTop: '5px', paddingLeft:'170px'}}>Genres</h4>
             </div>
-        <div className="container-fluid pt-0 mx-auto" id="genres" style={{width:'80%'}}>
+        <div className="container">
             <div className="row">
-                <div className="col" style={{border:'1px solid black', paddingLeft:'0px', paddingRight:'0px'}}>
-                    <ul className="list-group list-group-horizontal">
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_A">                    
+                <div className="col px-2">
+                    <ul className="list-group list-group-horizontal" style={{border:'1px solid black'}}>
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_A">                    
                             <h6>A</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_B" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_B" >                    
                             <h6 >B</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_C" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_C" >                    
                             <h6 >C</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_D" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_D" >                    
                             <h6 >D</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_E" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_E" >                    
                             <h6 >E</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_F" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_F" >                    
                             <h6 >F</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_G" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_G" >                    
                             <h6 >G</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_H" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_H" >                    
                             <h6 >H</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_I" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_I" >                    
                             <h6 >I</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_J" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_J" >                    
                             <h6 >J</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_K" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_K" >                    
                             <h6 >K</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_L" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_L" >                    
                             <h6 >L</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_M" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_M" >                    
                             <h6 >M</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_N" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_N" >                    
                             <h6 >N</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_O" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_O" >                    
                             <h6 >O</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_P" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_P" >                    
                             <h6 >P</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_Q" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_Q" >                    
                             <h6 >Q</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_R" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_R" >                    
                             <h6 >R</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_S" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_S" >                    
                             <h6 >S</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_T" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_T" >                    
                             <h6 >T</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_U" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_U" >                    
                             <h6 >U</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_V" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_V" >                    
                             <h6 >V</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_W" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_W" >                    
                             <h6 >W</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_X" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_X" >                    
                             <h6 >X</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_Y" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_Y" >                    
                             <h6 >Y</h6>
                         </li>
-                        <li className="list-group-item" data-toggle="collapse" href="#genres_Z" >                    
+                        <li className="list-group-item px-3" data-toggle="collapse" href="#genres_Z" >                    
                             <h6 >Z</h6>
                         </li>
                     </ul>   
